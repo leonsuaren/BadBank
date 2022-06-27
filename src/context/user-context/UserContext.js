e73b74ffe286237722242d04acec84087b893fe0
Leon Suarez <leonsua@gmail.com>
2022-06-27 08:28:23 -0400

    added email verification

--- src/context/user-context/UserContext.js
+++ src/context/user-context/UserContext.js
@@ -6,7 +6,6 @@ export const UserContextProvider = ({ children }) => {
   const [userBalance, setUserBalance] = useState(100);
   const [user, setUser] = useState({});
   const [userLogin, setUserLogin] = useState(false);
-
   return (
     <UserContext.Provider value={{ userBalance, user, userLogin, setUserBalance, setUser, setUserLogin }}>
       { children }
--- src/views/deposit/Deposit.jsx
+++ src/views/deposit/Deposit.jsx
@@ -4,23 +4,27 @@ import { UserContext } from '../../context/user-context';
 import { Card } from '../../components/card';
 
 export const Deposit = () => {
-  const [show, setShow] = useState(true);
-  const [deposit, setDeposit] = useState();
-  const [status, setStatus] = useState('');
   const userContext = useContext(UserContext);
+  const [show, setShow] = useState(true);
+  const [error, setError] = useState('');
   const userBalance = userContext.user.balance;
-  console.log(userContext.user);
-
+  const [deposit, setDeposit] = useState(0);
+  const [inicialBalance, setInicialBalance] = useState(userBalance);
+  const [balance, setBalance] = useState(0);
+  const [validateForm, setValidateForm] = useState(false);
+  console.log(deposit);
   function handleDeposit() {
     if (deposit <= 0) {
       setDeposit(0);
-      setStatus('Please Enter a Valid Deposit');
+      setValidateForm(true);
+      setError('Deposit amount must be greater that 0');
       setTimeout(() => {
-        setStatus('');
+        setError('');
+        setValidateForm(false);
       }, 3000);
       return
     }
-    userContext.setUserBalance(s => s + Number(deposit));
+    setBalance(Number(inicialBalance) + Number(deposit));
     setShow(false);
   }
 
@@ -29,28 +33,36 @@ export const Deposit = () => {
   }
 
   return (
-    <Card
-      bgcolor='success'
-      header='Deposit'
-      text={'Balance = ' + userBalance}
-      status={status}
-      body={show ? (
-        <div>
-          Deposti Amount<br />
-          <input type="number" className="form-control" id="deposit" placeholder="Enter Deposit Amount" value={deposit} onChange={e => setDeposit(e.currentTarget.value)}
-          /><br />
-          <button type="submit" className="btn btn-light" onClick={handleDeposit}
-          >Deposit</button>
-        </div>
-      ) : (
+    <div className='container alert-container'>
+      <Card
+        bgcolor='success'
+        header='Deposit'
+        text={'Balance = ' + balance}
+        body={show ? (
           <div>
-          Deposit Success<br />
-          <button type="submit" className="btn btn-light" onClick={handleAnotherDeposit}
-          >Another Deposit</button>
+            Deposti Amount<br />
+            <input type="number" className="form-control" id="deposit" placeholder="Enter Deposit Amount" value={deposit} onChange={e => setDeposit(e.currentTarget.value)}
+            /><br />
+            <button type="submit" className="btn btn-light" onClick={handleDeposit}
+            >Deposit</button>
           </div>
-        )
+        ) : (
+            <div>
+              Deposit Success<br />
+              <button type="submit" className="btn btn-light" onClick={handleAnotherDeposit}
+              >Another Deposit</button>
+            </div>
+          )
 
+        }
+      />
+      {
+        validateForm ?
+          <div className="alert alert-warning" role="alert">
+            {error}
+          </div>
+          : ''
       }
-    />
+    </div>
   )
 }
