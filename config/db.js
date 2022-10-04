a6390a3b4979d92911ad303ad13b6ef5d3c16a46
Leon Suarez <leonsua@gmail.com>
2022-10-04 14:25:41 -0400

    change the mongodb process env variable name from url to uri

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URL || `mongodb+srv://leonsuarez:badbank@badbank.mn7kwbo.mongodb.net/?retryWrites=true&w=majority`, {
-    useNewUrlParser: true
+  await mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://leonsuarez:badbank@badbank.mn7kwbo.mongodb.net/?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   });
   console.log('MongoDB connect');
 }
