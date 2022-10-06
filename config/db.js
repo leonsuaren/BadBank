6d30bcabe2e528b718d88b2e49c88dd5de21abd1
Leon Suarez <leonsua@gmail.com>
2022-10-06 17:50:32 -0400

    use env. to protect mongo credentials

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://leonsuarez:badbank@badbank.mn7kwbo.mongodb.net/?retryWrites=true&w=majority`, {
+  await mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_PASSWORD}@badbank.mn7kwbo.mongodb.net/?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true 
   });
